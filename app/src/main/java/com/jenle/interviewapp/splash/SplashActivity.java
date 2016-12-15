67a1eff5602822f8e654a1e4d4a2c38ac6c8bff6
Jenle Samuel <jenlesamuel@gmail.com>
2016-12-15 12:21:36 +0100

    Added splash screen

--- app/src/main/java/com/jenle/interviewapp/splash/SplashActivity.java
+++ app/src/main/java/com/jenle/interviewapp/splash/SplashActivity.java
@@ -1,7 +1,13 @@
 package com.jenle.interviewapp.splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jenle.interviewapp.R;
+import com.jenle.interviewapp.Utils;
+import com.jenle.interviewapp.evaluate.view.EvaluationActivity;
+import com.jenle.interviewapp.login.view.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Check if user has a valid session and redirect as necessary
+        String token = new Utils().getStringFromPreference(this, getResources().getString(R.string.token));
+        if (token != null) {
+            startActivity(new Intent(this, EvaluationActivity.class));
+        }else{
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        finish();
     }
 }
